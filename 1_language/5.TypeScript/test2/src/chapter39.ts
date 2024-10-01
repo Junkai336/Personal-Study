4ee497cd0d7d4574308e488709de83bc19d33116
Junkai336 <gjwnssla2@gmail.com>
2024-10-02 04:06:48 +0900

    1002

--- 1_language/5.TypeScript/test2/src/chapter39.ts
+++ 1_language/5.TypeScript/test2/src/chapter39.ts
@@ -21,3 +21,17 @@ type B = Extract<string | boolean, boolean>;
 
 
 // ReturnType
+// 함수의 반환값 타입을 추출하는 타입
+type ReturnType<T extends (...args : any) => any> = T extends (...args : any) => infer R ? R : never; // T 타입이 서브타입이 되도록 하는 R을 추론하라.
+
+function funcA() {
+  return "hello";
+}
+
+function funcB() {
+  return 10;
+}
+
+type ReturnA = ReturnType<typeof funcA>;
+
+type ReturnB = ReturnType<typeof funcB>;  // funcB의 리턴타입이 number라서 ReturnB가 number 타입이 됨
