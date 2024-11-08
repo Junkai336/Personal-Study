e12903c7a65f3563155bd6b9ca2c47ec4cc62cd5
Junkai336 <gjwnssla2@gmail.com>
2024-11-08 23:47:32 +0900

    1108

--- 2_libraryframework/4.React/test4/src/App.jsx
+++ 2_libraryframework/4.React/test4/src/App.jsx
@@ -1,30 +1,42 @@
 import './App.css';
-import Footer from './components/Footer.jsx';
-import Header from './components/Header.jsx';
-import Main from './components/Main.jsx';
-import Button from './components/Button.jsx';
+import { useState } from 'react';
 
-function App() {
-  // 스프레드 연산자 이용
-  const buttonProps = {
-    text: "메일",
-    color: "red",
-    a: 1,
-    b: 2,
-    c: 3
-  }
+const Bulb = () => {
+  const [light, setLight] = useState("OFF");
+
+  console.log(light);
+  return (
+    <div>
+      {light === "ON" ? <h1 style={{ backgroundColor: "orange" }}>ON</h1> : <h1 style={{ backgroundColor: "gray" }}>OFF</h1>}
+
+      <button
+        onClick={() => { setLight(light === "ON" ? "OFF" : "ON") }}
+      >켜기/끄기
+      </button>
+    </div>
+  );
+}
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
 
   return (
+    <div>
+      <h1>{count}</h1>
+      <button
+        onClick={() => { setCount(count + 1); }}
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+function App() {
+  return (
     <>
-      {/* props를 전달해 주면 이 값들은 객체로 묶여서 자식 컴포넌트의 매개변수로 제공된다. */}
-      <Button {...buttonProps} />
-      <Button text={"카페"} color={"orange"}/>
-      <Button text={"블로그"}/>
-      <Button text={"ㅎㅇ"}>
-        <div>자식요소, children이란 props로 전달됨</div>
-        <div>자식요소들은 자동으로 전달됨</div>
-        <Header />
-      </Button>
+      <Bulb />
+      <Counter />
     </>
   )
 }
