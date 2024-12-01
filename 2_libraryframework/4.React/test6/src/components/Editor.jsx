6a25834cf841465b3b611efff2c6b138ddea77f5
Junkai336 <gjwnssla2@gmail.com>
2024-12-01 23:10:22 +0900

    1201

--- 2_libraryframework/4.React/test6/src/components/Editor.jsx
+++ 2_libraryframework/4.React/test6/src/components/Editor.jsx
@@ -1,10 +1,40 @@
+import { useState, useRef } from "react";
 import "./Editor.css"
 
-const Editor = () => {
+const Editor = ({onCreate}) => {
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+
+  const onChangeContent = (e) => {
+    setContent(e.target.value);
+  }
+
+  const onSubmit = () => {
+    if(content === '') {
+      alert("한 단어 이상 입력해 주세요.");
+      contentRef.current.focus();
+      return;
+    }
+    onCreate(content);
+    setContent("");
+  }
+
+  // 사용자가 onKeyDown 키보드를 눌렀을 때 발생
+  const onKeydown = (e) => {
+    if (e.keyCode === 13) {
+      onSubmit();
+    }
+  }
+
   return (
     <div className="Editor">
-      <input placeholder="새로운 Todo..." />
-      <button>추가</button>
+      <input 
+        value={content}
+        ref={contentRef} 
+        onKeyDown={onKeydown}
+        onChange={onChangeContent}  
+        placeholder="새로운 Todo..." />
+      <button onClick={onSubmit}>추가</button>
     </div>
   )
 }
--- 2_libraryframework/4.React/test6/src/components/List.jsx
+++ 2_libraryframework/4.React/test6/src/components/List.jsx
@@ -1,15 +1,42 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
+import { useState } from "react";
 
-const List = () => {
+// 배열에 담긴 값을 리스트 형태로 반복적으로 렌더링하려면 map 메소드를 이용하면 됨
+// 하나의 콜백함수를 받은 다음 배열의 모든 요소에 대해서 콜백 함수를 수행한 뒤 콜백 함수가 리턴한 반환 값들을 모아서 새로운 배열로 반환
+// map 메소드를 jsx에서 사용할 때 콜백 함수가 HTML 요소를 반환하도록 할 수 있음
+
+// 
+
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  }
+
+  // 리렌더링 되면서 다시 읽는 듯?
+  const getFilteredData = () => {
+    if (search === "") {
+      return todos;
+    }
+    return todos.filter((todo) => { todo.content.toLowerCase().includes(search.toLowerCase()); })
+  }
+
+  const filteredTodos = getFilteredData();
+
   return (
     <div className="List">
       <h4>Todo List 👌</h4>
-      <input placeholder="검색어를 입력하세요." />
+      <input value={search} onChange={onChangeSearch} placeholder="검색어를 입력하세요." />
       <div className="todos_wrapper">
-        <TodoItem />
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return (
+            <div>
+              <TodoItem key={todo.id} {...todo} onUpdate={onUpdate} onDelete={onDelete} />
+            </div>
+          )
+        })}
       </div>
     </div>
   )
