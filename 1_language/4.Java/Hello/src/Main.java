16a5a307dfd6b5f3b7bab4495ef49e75cdcecadc
Junkai336 <gjwnssla2@gmail.com>
2025-01-30 23:53:00 +0900

    0130

--- 1_language/4.Java/Hello/src/Main.java
+++ 1_language/4.Java/Hello/src/Main.java
@@ -3,28 +3,34 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    int dp[][] = new int[41][2];
+        int n = Integer.parseInt(br.readLine());
 
-    dp[0][0] = 1;
-    dp[0][1] = 0;
-    dp[1][0] = 0;
-    dp[1][1] = 1;
+        int[] arr = new int[n];
+        int[] dp = new int[n];
 
-    for(int i = 2; i < 41; i++) {
-      dp[i][0] = dp[i-1][0] + dp[i-2][0];
-      dp[i][1] = dp[i-1][1] + dp[i-2][1];
+        String[] st = br.readLine().split(" ");
+        for (int i = 0; i < st.length; i++) {
+            arr[i] = Integer.parseInt(st[i]);
+            dp[i] = arr[i];
+        }
+
+        for (int i = 1; i < n; i++) {           // 현재 원소
+            for (int j = 0; j < i; j++) {       // 이전 원소
+                if (arr[i] > arr[j]) {          
+                    dp[i] = Math.max(dp[i], dp[j] + arr[i]);
+                }
+            }
+        }
+
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+
+        System.out.println(max);
+
     }
-
-    int T = Integer.parseInt(br.readLine());
-
-    for (int i = 0; i < T; i++) {
-      int N = Integer.parseInt(br.readLine());
-
-      System.out.println(dp[N][0] + " " + dp[N][1]);
-    }
-
-  }
 }
